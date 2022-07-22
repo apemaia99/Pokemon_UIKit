018e01f7fedd85af0b9a20ad2ea59199aa4f5e52
Alessando Di Maio <92519597+apemaia99@users.noreply.github.com>
2022-07-22 10:38:26 +0200

    Initial Commit

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Alessandro Di Maio on 22/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
